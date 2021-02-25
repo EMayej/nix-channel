0dd2155468aadf7c6e6af8c28febdb23223f57cc
EMayej <EMayej@gmail.com>
2021-02-25 22:04:28 +0800

    feat: build package adguard-exporter

---
+++ nix/adguard-exporter.nix
@@ -0,0 +1,25 @@
+{ pkgs, lib, buildGoModule }:
+
+let
+  sources = import ./sources.nix;
+  adguard-exporter = sources.adguard-exporter;
+  version = lib.removePrefix "v" adguard-exporter.rev;
+in buildGoModule {
+  pname = "adguard-exporter";
+  inherit version;
+
+  src = adguard-exporter;
+
+  vendorSha256 = "15bkw31lna648ynq3h0wvd4w60jaci33kr0mp4919rk42ybyb8pw";
+
+  doCheck = false;
+
+  subPackages = [ "." ];
+
+  meta = with lib; {
+    inherit (adguard-exporter) description homepage;
+    license = licenses.mit;
+    platforms = platforms.linux ++ platforms.darwin;
+  };
+  overrideModAttrs = (_: { GOPROXY = "https://goproxy.cn,direct"; });
+}
--- nix/default.nix
+++ nix/default.nix
@@ -3,4 +3,5 @@
 {
   gost = pkgs.callPackage ./gost.nix { };
   adguardhome = pkgs.callPackage ./adguardhome.nix { };
+  adguard-exporter = pkgs.callPackage ./adguard-exporter.nix { };
 }
